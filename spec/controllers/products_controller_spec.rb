2a01151b8fa4c83866365b0c959cbee69aed453d
tgaff <tgaff@alumni.nd.edu>
2016-01-22 19:47:53 -0800

    Update spec to more recent guidelines

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -3,55 +3,52 @@ require "rails_helper"
 RSpec.describe ProductsController, type: :controller do
 
   describe "#index" do
-    before do
-      @all_products = Product.all
-      get :index
-    end
+    let(:all_products) { Product.all }
+    before { get :index }
 
-    it "should assign @products" do
+    it "assigns @products" do
       expect(assigns(:products)).to eq(@all_products)
     end
 
-    it "should render the :index view" do
+    it "renders the :index view" do
       expect(response).to render_template(:index)
     end
   end
 
   describe "#new" do
-    before do
-      get :new
-    end
+    before { get :new }
 
-    it "should assign @product" do
+    it "assigns @product" do
       expect(assigns(:product)).to be_instance_of(Product)
     end
 
-    it "should render the :new view" do
+    it "renders the :new view" do
       expect(response).to render_template(:new)
     end
   end
 
   describe "#create" do
-    context "success" do
+    context "on success" do
+
+      let!(:products_count) { Product.count }
+
       before do
-        @products_count = Product.count
-        wholesale_price = Random.new.rand(1.0..100.0).round(2)
-
-        post :create, product: {
-          name: FFaker::Lorem.words(5).join(" "),
-          description: FFaker::Lorem.sentence,
-          category: FFaker::Lorem.words(3).join,
-          sku: FFaker::Lorem.words(2).join,
-          wholesale: wholesale_price,
-          retail: wholesale_price * 4
-        }
+        product_hash = {
+            name: FFaker::Lorem.words(5).join(" "),
+            description: FFaker::Lorem.sentence,
+            category: FFaker::Lorem.words(3).join,
+            sku: FFaker::Lorem.words(2).join,
+            wholesale: Random.new.rand(1.0..100.0).round(2),
+            retail: wholesale_price * 4
+          }
+        post :create, product_hash
       end
 
-      it "should add new product to the database" do
-        expect(Product.count).to eq(@products_count + 1)
+      it "adds the new product to the database" do
+        expect(Product.count).to eq(products_count + 1)
       end
 
-      it "should redirect_to 'product_path'" do
+      it "redirects to 'product_path'" do
         expect(response.status).to be(302)
         expect(response.location).to match(/\/products\/\d+/)
       end
@@ -70,11 +67,11 @@ RSpec.describe ProductsController, type: :controller do
         }
       end
 
-      it "should display an error message" do
+      it "displays a flash error message" do
         expect(flash[:error]).to be_present
       end
 
-      it "should redirect to 'new_product_path'" do
+      it "redirects to 'new_product_path'" do
         expect(response.status).to be(302)
         expect(response).to redirect_to(new_product_path)
       end
@@ -82,28 +79,29 @@ RSpec.describe ProductsController, type: :controller do
   end
 
   describe "#show" do
+    let(:product) { FactoryGirl.create(:product) }
+
     before do
-      @product = FactoryGirl.create(:product)
-      get :show, id: @product.id
+      get :show, id: product.id
     end
 
-    it "should assign @product" do
-      expect(assigns(:product)).to eq(@product)
+    it "assigns @product" do
+      expect(assigns(:product)).to eq(product)
     end
 
-    it "should render the :show view" do
+    it "renders the :show view" do
       expect(response).to render_template(:show)
     end
   end
 
   describe "#edit" do
+    let(:product) { FactoryGirl.create(:product) }
     before do
-      @product = FactoryGirl.create(:product)
-      get :edit, id: @product.id
+      get :edit, id: product.id
     end
 
     it "should assign @product" do
-      expect(assigns(:product)).to eq(@product)
+      expect(assigns(:product)).to eq(product)
     end
 
     it "should render the :edit view" do
@@ -112,51 +110,49 @@ RSpec.describe ProductsController, type: :controller do
   end
 
   describe "#update" do
-    before do
-      @product = FactoryGirl.create(:product)
-    end
+    let(:product) { FactoryGirl.create(:product) }
 
     context "success" do
+      let(:new_name) { FFaker::Lorem.words(5).join(" ") }
+      let(:new_description) { FFaker::Lorem.sentence }
+      let(:new_category) { FFaker::Lorem.words(3).join }
+      let(:new_sku) { FFaker::Lorem.words(2).join }
+      let(:new_wholesale) { Random.new.rand(1.0..100.0).round(2) }
+      let(:new_retail) { new_wholesale * 4 }
+
       before do
-        @new_name = FFaker::Lorem.words(5).join(" ")
-        @new_description = FFaker::Lorem.sentence
-        @new_category = FFaker::Lorem.words(3).join
-        @new_sku = FFaker::Lorem.words(2).join
-        @new_wholesale = Random.new.rand(1.0..100.0).round(2)
-        @new_retail = @new_wholesale * 4
-
-        put :update, id: @product.id, product: { 
-          name: @new_name,
-          description: @new_description,
-          category: @new_category,
-          sku: @new_sku,
-          wholesale: @new_wholesale,
-          retail: @new_retail
+        put :update, id: product.id, product: {
+          name: new_name,
+          description: new_description,
+          category: new_category,
+          sku: new_sku,
+          wholesale: new_wholesale,
+          retail: new_retail
         }
-        
+
         # reload @product to get changes from :update
-        @product.reload
+        product.reload
       end
 
-      it "should update product in the database" do
-        expect(@product.name).to eq(@new_name)
-        expect(@product.description).to eq(@new_description)
-        expect(@product.category).to eq(@new_category)
-        expect(@product.sku).to eq(@new_sku)
-        expect(@product.wholesale).to eq(@new_wholesale)
-        expect(@product.retail).to eq(@new_retail)
+      it "updates the product in the database" do
+        expect(product.name).to eq(new_name)
+        expect(product.description).to eq(new_description)
+        expect(product.category).to eq(new_category)
+        expect(product.sku).to eq(new_sku)
+        expect(product.wholesale).to eq(new_wholesale)
+        expect(product.retail).to eq(new_retail)
       end
 
-      it "should redirect_to 'product_path'" do
+      it "redirects to 'product_path'" do
         expect(response.status).to be(302)
-        expect(response).to redirect_to(product_path(@product))
+        expect(response).to redirect_to(product_path(product))
       end
     end
 
     context "failed validations" do
       before do
         # update with blank product params (fails validations)
-        put :update, id: @product.id, product: { 
+        put :update, id: @product.id, product: {
           name: nil,
           description: nil,
           category: nil,
@@ -166,28 +162,29 @@ RSpec.describe ProductsController, type: :controller do
         }
       end
 
-      it "should display an error message" do
+      it "displays an error message" do
         expect(flash[:error]).to be_present
       end
 
-      it "should redirect_to 'edit_product_path'" do
+      it "redirects to 'edit_product_path'" do
         expect(response).to redirect_to(edit_product_path(@product))
       end
     end
   end
 
   describe "#destroy" do
+    let!(:products_count) { Product.count }
+    let(:product) { FactoryGirl.create(:product) }
+
     before do
-      product = FactoryGirl.create(:product)
-      @products_count = Product.count
       delete :destroy, id: product.id
     end
 
-    it "should remove product from the database" do
-      expect(Product.count).to eq(@products_count - 1)
+    it "removes the product from the database" do
+      expect(Product.count).to eq(products_count - 1)
     end
 
-    it "should redirect_to 'root_path'" do
+    it "redirects to 'root_path'" do
       expect(response.status).to be(302)
       expect(response).to redirect_to(root_path)
     end
