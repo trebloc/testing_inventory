9cec314bc2b6a54c1e4bcc1c8cc09490bc79b1b9
tgaff <tgaff@alumni.nd.edu>
2016-01-23 19:17:21 -0800

    update model specs to current preferred styles

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,33 +1,33 @@
 require "rails_helper"
 
 RSpec.describe Product, type: :model do
-  
+
   describe "methods" do
     describe "#margin" do
-      before do
-        @product = FactoryGirl.create(:product)
-        @retail_margin = (@product.retail - @product.wholesale) / @product.retail
-      end
+      let(:product) { FactoryGirl.create(:product) }
 
       it "should calculate product's retail margin" do
-        expect(@product.margin).to eq(@retail_margin)
+        retail_margin = (product.retail - product.wholesale) / product.retail
+        expect(product.margin).to eq(retail_margin)
       end
     end
 
     describe "sell_through" do
+      let(:product) { FactoryGirl.create(:product) }
+
       before do
-        @product = FactoryGirl.create(:product)
-        5.times do
-          random_status = ["in", "out", "sold", "clearanced"].sample
-          @product.items.create(size: "M", color: "burgundy", status: random_status)
+        statuses = ["sold", "in", "out", "sold", "clearanced"]
+        statuses.each do |status|
+          product.items.create(size: "M", color: "burgundy", status: status)
         end
-        total_items = @product.items.count
-        items_sold = @product.items.where(status: "sold").count
+
+        total_items = product.items.count
+        items_sold = product.items.where(status: "sold").count
         @sell_through = items_sold / total_items
       end
 
       it "should calculate product's overall sell-through rate" do
-        expect(@product.sell_through).to eq(@sell_through)
+        expect(product.sell_through).to eq(@sell_through)
       end
     end
   end
